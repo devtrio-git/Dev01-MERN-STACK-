54525e61d143a9730ff32721ab5b14fbe8bed7f0
ahmedashar <ahmedashar12345@gmail.com>
2024-02-04 02:00:43 +0500

    addClass09

---
+++ Class09(JS)/app.js
@@ -0,0 +1,119 @@
+// 1. printing, console.log() , document.write(), alert and prompt
+// console.log("Hello World2")
+
+// console.log(24)
+// document.write("AShar Ahmed") 
+
+// console.log(Hello);
+
+// camelCase and PascalCase (userName and UserName)
+
+// 2. data types and variable
+// String "AShar", 'Ashar'
+// Number  12 or 52 
+// Boolean true and false
+
+// var userName = "Ashar Ahmed";
+// var userAge = 24;
+// var isStudent = false;
+
+// var xy; // only declare not initialize (not assign value)
+
+// console.log(userName);
+// console.log(userAge);
+// console.log(isStudent);
+
+// console.log(typeof(userName));
+// console.log(typeof(userAge));
+// console.log(typeof(isStudent));
+
+
+// concatenation--> putting things together
+
+// my name is Ashar Ahmed
+
+// var firstName = "Ashar";
+// var secondName = "Ahmed";
+// var num1 = 12;
+// var num2 = 10;
+
+// console.log(firstName)
+
+// console.log(firstName + secondName);
+
+// console.log(num1+num2);
+
+// console.log(firstName + num1);
+
+// console.log("my name is "+ firstName  + " " +  secondName);
+
+//alert:  talking to the user (generate popup)
+// console.log(1);
+// alert("Welcome to our Website");
+// alert("my name is "+ firstName  + " " +  secondName);
+// console.log(2);
+
+// prompt : Getting information from the user || prompt return string
+
+// var name = prompt("Enter Your Name");
+// alert(name)
+
+
+// 1. welcome
+// 2. get first name
+// 3. get second name
+// 4. concate and show fullname
+
+// alert("Welcome to our website");
+
+// var userFirstName = prompt("Enter Your First Name");
+// var userSecondName = prompt("Enter your Second Name");
+
+// alert("Your Full Name is " + userFirstName + " "+ userSecondName);
+
+
+// 1. welcome
+// 2. get first number
+// 3. get second number
+// 4. addition and show result
+
+
+// alert("Welcome to our website");
+
+// var num1 = +prompt("Enter First Number");
+
+// var num2 = +prompt("Enter Second Number");
+
+// alert(num1 + num2)
+
+
+// var --> changed and reDeclare
+// let --> changed, cannot reDeclare
+// const --> channot, cannot reDeclares
+
+var studentName = "Ibrahim";
+
+studentName = "Anas" // change
+var studentName = "Ashar" // re-declare
+
+
+let secondName = "Owais";
+secondName = "Ahmed"; // change
+// let secondName = "Ashar"; // cannot re-declare
+
+const thirdName = "Karim";
+// thirdName = "asdasd"; cannot re-assign 
+
+console.log(thirdName)
+
+/*
+Table of content
+
+Concept of JS (Intro, history, features, uses)
+Console.log(), document.write()
+variable with data types
+concatenation
+alert()
+prompt() with unary operator
+var, let and const
+*/
