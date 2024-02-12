8fda200fe52b12a95fdb0841a0186e1bd408cc35
Owaisk23 <owais1566@gmail.com>
2024-02-12 05:55:02 +0500

    addedClass11

---
+++ Class11(JS)/app.js
@@ -0,0 +1,144 @@
+// console.log("Hello world!")
+
+// ARRAYS denote square brackets[]
+// indexes always start 0
+// students data
+
+// var std1 = "Abdul Karim"
+// var std2 = "Husnain"
+// var std3 = "Husnain"
+
+
+// var students = ["Karim", "Husnain", "Ali", "Owais", "Ayesha", "Khan", "Jahangir", "Razi"]
+
+// console.log(students)
+
+// calling array with index
+// console.log(students[6])
+// console.log(students[5])
+
+// var foods = ["Burger", "Biryani", "Karhai", "Pizza"];
+
+// var foods = new Array("Burger", "Biryani", "Karhai", "Pizza")
+
+// console.log(foods)
+
+// multiple datatypes in array
+// string
+// var a = ["Hello", "World", "Devtrio"];
+
+// var b = [23, 45, 55, 77, 99];
+
+// var c = [true, false, true, false];
+
+// var d = [24.55, 99.45, 77.2];
+
+// var mixDt = ["Owais", 23, 5.11, true];
+
+// var arrObj = [{name: "Owais"}, {name: "Malik"}]
+// obj {name: owais}
+
+// console.log(typeof(mixDt))
+// var parties = ["PPP","PMNL","IND","PTI", "MQM", "JI", "Kaff"]
+
+
+// console.log(parties[5])
+// console.log(parties.length)
+
+// for(var i = 0; i < parties.length; i++){
+//     console.log("Election parties " + parties[i])
+// }
+
+
+
+// Array methods
+
+// push()
+// add elements at last
+
+// var marks = [98, 88, 77, 55, 84];
+
+// console.log(marks)
+
+// marks.push(90, 88, 91)
+
+// console.log(marks)
+
+//shift()
+// there is no arr
+// Remove elemnt from start 
+
+// var cities = ["Khi", "lhr", "Isb", "Multan"];
+
+// console.log(cities)
+
+// cities.shift();//khi
+// console.log(cities); 
+
+// cities.shift();//lhr
+
+// console.log(cities);
+
+// unshift()
+// add elemnt at start
+
+// console.log(cities);
+
+// var a = "SWATT"
+
+// // cities.unshift("Quetta")
+// // cities.unshift("Quetta", "Swat", "RahimyrKhan")
+
+// cities.unshift(a)
+
+// console.log(cities);
+
+
+// pop()
+// there is no arr
+// remove elemnt from last
+
+// var temp = [23 ,55, 17, 22, 40, 52]
+
+// console.log(temp)
+
+// temp.pop()
+
+// console.log(temp)
+
+
+// slice()
+// slice(startIndex, EndLength)
+// var num = [100, 89, 55, 66, 77, 55, 78, 22, 55, 44, 50];
+
+
+// console.log(num)
+
+// var sliceArray = num.slice(1, 7);
+
+// console.log(sliceArray)
+
+// splice()
+// splice(startIndex, "DeletCount", "AddElemnt")
+// var fruits = ["Apple", "Orange", "Banana", "Kiwi", "Peach", "Grapes"]
+
+// console.log(fruits)
+
+
+// var spliceArray = fruits.splice(1, 3, "Mango", "Watermelon")
+
+
+// console.log(fruits)
+
+ // temp literal
+        // var fname = "Owais Ahmed";
+        // var lname = "Khan"
+
+        // console.log("My name is " + fname + " Last name is " + lname)
+
+        // console.log(`My name is ${fname} last name is ${lname}`)
+
+
+
+
+
